b9576ca9df395627ac3500ee52edafc4eebac628
Glenn Stovall <glennstovall@gmail.com>
2014-09-30 17:08:20 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iditWireframesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the iditWireframesApp
+ */
+angular.module('iditWireframesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
