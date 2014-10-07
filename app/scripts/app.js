579891c16a3e7178deb9d67c17b5589aff742407
Glenn Stovall <glennstovall@gmail.com>
2014-10-07 14:00:07 -0400

    refactored common views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,44 +22,10 @@ angular
       .when('/', {
         templateUrl: 'views/main.html'
       })
-      .when('/listings', {
-        templateUrl: 'views/listings.html'
-      })
-      .when('/listing-profile', {
-        templateUrl: 'views/listing-profile.html'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html'
-      })
-      .when('/terms-of-service', {
-        templateUrl: 'views/terms-of-service.html'
-      })
-      .when('/privacy-policy', {
-        templateUrl: 'views/privacy-policy.html'
-      })
-      .when('/contact',  {
-        templateUrl: 'views/contact.html'
-      })
-      .when('/poster-login', {
-        templateUrl: 'views/poster-login.html'
-      })
-      .when('/expert-login', {
-        templateUrl: 'views/expert-login.html'
-      })
-      .when('/poster-signup', {
-        templateUrl: 'views/poster-signup.html'
-      })
-      .when('/expert-signup', {
-        templateUrl: 'views/expert-signup.html'
-      })
-      .when('/forgot-password', {
-        templateUrl: 'views/forgot-password.html'
-      })
-      .when('/poster-dashboard', { //poster account pages
-        templateUrl: 'views/poster-dashboard.html'
-      })
-      .when('/create-listing', {
-        templateUrl: 'views/create-listing.html'
+      .when('/:page', {
+        templateUrl: function(urlattr) {
+          return 'views/' + urlattr.page + '.html'
+        }
       })
       .otherwise({
         redirectTo: '/'
